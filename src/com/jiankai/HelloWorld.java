db45f4f1b848c5ca3dfc1d17861f78a4873ce08a
jiankai <jiankailv@foxmail.com>
2020-01-14 20:05:03 +0800

    第二个人第二次更新了代码

--- src/com/jiankai/HelloWorld.java
+++ src/com/jiankai/HelloWorld.java
@@ -8,5 +8,9 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("hello world hello 222");
+		System.out.println("hello ");
+	}
+	public void test(){
+		System.out.println("123456");
 	}
 }
